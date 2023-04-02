6cfd763d6bcb55bfc81f101d63d8a0602aa305b9
MuhdHanish <muhammedhanish11@gmail.com>
2023-04-02 20:30:43 +0530

    First Push After Host

--- controller/prouductController.js
+++ controller/prouductController.js
@@ -17,7 +17,7 @@ module.exports = {
 
   getHomeProducts: async (req, res, next) => {
     try {
-      const products = await productCollection.find({}, { sort: { _id: -1 } }).limit(4).toArray()
+      const products = await productCollection.find({}).sort({_id:-1}).limit(4).toArray()
       const banners = await bannerCollection.find({ status: true }).skip(1).toArray()
       const firstBanner = await bannerCollection.find({ status: true }).limit(1).toArray()
       req.session.successId = null
